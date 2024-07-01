a9f854f78ebc1a05eb06a50ead8406327b000b2e
John Crooks <john.crooks.a@gmail.com>
2024-06-30 17:27:00 -0700

    Nvim: added colorscheme plugin

---
+++ .config/nvim/lua/jcrooks/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+
+return {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,  -- Make sure to load this before all the other start plugins.
+    init = function()
+	    -- Config (must run before loading colorscheme)
+	    vim.g.everforest_background = "hard"
+	    vim.g.everforest_transparent_background = true
+	    vim.g.everforest_enable_italic = true
+	    vim.cmd.hi 'Comment gui=none'  -- highlights
+
+	    -- Load
+	    vim.cmd.colorscheme("everforest")
+
+    end,
+  }
+

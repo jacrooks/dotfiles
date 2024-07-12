300caee09db44db785a4121b5961546cc32f7a7f
John Crooks <john.crooks.a@gmail.com>
2024-07-12 15:48:37 -0700

    Wezterm: removed padding at bottom of window

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,5 +8,6 @@ config.macos_window_background_blur = 30
 config.window_background_opacity = 0.90
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = { bottom = "0px" }
 
 return config

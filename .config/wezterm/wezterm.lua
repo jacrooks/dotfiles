f23d2cc77970bd4162ab1d2d2c9a3527baa25551
John Crooks <john.crooks.a@gmail.com>
2024-07-12 11:43:20 -0700

    Wezterm: only show tab bar if more than one tab exists

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,15 +1,12 @@
-
-local wezterm = require "wezterm"
+local wezterm = require("wezterm")
 local config = {}
 
-
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font('JetBrains Mono')
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 14.0
 config.macos_window_background_blur = 30
 config.window_background_opacity = 0.90
-config.window_decorations = 'RESIZE'
-
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
-

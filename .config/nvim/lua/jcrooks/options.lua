e562999a752314c215a43bbda8f56f6f1a7ed091
John Crooks <john.crooks.a@gmail.com>
2024-07-14 14:08:21 -0700

    Nvim: changes scroll line option to 5 lines

--- .config/nvim/lua/jcrooks/options.lua
+++ .config/nvim/lua/jcrooks/options.lua
@@ -1,4 +1,3 @@
-
 -- Set core neovim options
 -- ------------------------------------------------------------------------------
 -- See kickstart.nvim for more info
@@ -7,23 +6,23 @@
 
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 vim.opt.showmode = false
-vim.opt.clipboard = 'unnamedplus'  -- sync clipboard between OS and Neovim.
+vim.opt.clipboard = "unnamedplus" -- sync clipboard between OS and Neovim.
 vim.opt.breakindent = true
 vim.opt.undofile = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
 vim.opt.cursorline = true
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 5
 vim.opt.wrap = false
 
 vim.opt.tabstop = 4
@@ -31,19 +30,17 @@ vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 vim.opt.expandtab = true
 
-
 -- Autocommands
 -- ------------------------------------------------------------------------------
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
-
 
 -- vim: ts=2 sts=2 sw=2 et

3eb0798849ce374e563f887205caf3eb2463cbbb
John Crooks <john.crooks.a@gmail.com>
2024-07-14 14:09:03 -0700

    Nvim: changed menu keymap to <leader>e

--- .config/nvim/lua/jcrooks/plugins/harpoon.lua
+++ .config/nvim/lua/jcrooks/plugins/harpoon.lua
@@ -13,7 +13,7 @@ return {
 		vim.keymap.set("n", "<leader>a", function()
 			harpoon:list():add()
 		end)
-		vim.keymap.set("n", "<C-e>", function()
+		vim.keymap.set("n", "<leader>e", function()
 			harpoon.ui:toggle_quick_menu(harpoon:list())
 		end)
 		vim.keymap.set("n", "<leader>1", function()

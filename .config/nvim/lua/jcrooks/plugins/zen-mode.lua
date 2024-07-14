6599726186d80d656d097ba2b2619edab12587d5
John Crooks <john.crooks.a@gmail.com>
2024-07-14 13:29:58 -0700

    Nvim: added zen-mode plugin

---
+++ .config/nvim/lua/jcrooks/plugins/zen-mode.lua
@@ -0,0 +1,10 @@
+-- Zen out while coding
+-- ------------------------------------------------------------------------------
+return {
+	"folke/zen-mode.nvim",
+	opts = {
+		plugins = {
+			tmux = { enabled = true },
+		},
+	},
+}
